8e762da049cedb440011debec02b08bb457c2025
Niklas Holtmeyer <4845193-niklasholtmeyer@users.noreply.gitlab.com>
2021-03-24 10:48:19 +0100

    mv

---
+++ setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+with open('LICENSE') as f:
+    license = f.read()
+    
+#with open('README.rst') as f:
+    #readme = f.read()
+    
+setup(
+    name='DeepSTT',
+    version='0.1.0',
+    description='',
+    #long_description=readme,
+    author='Niklas Holtmeyer',
+    url='https://github.com/NiklasHoltmeyer/Speech-to-Text/tree/master/',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    install_requires=[
+        "tqdm",
+        'pandas'],
+)
